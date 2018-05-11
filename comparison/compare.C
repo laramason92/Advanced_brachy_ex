2529a933846e6d19425faba914f2d700765c4e76
Lara Mason <lara.mason@live.com>
2018-05-11 09:27:13 +0200

    Ref Data

--- comparison/compare.C
+++ comparison/compare.C
@@ -1,19 +1,39 @@
 {
 // Read reference data in granero.txt
-FILE *fg1=fopen("granero.txt", "r");
-Int_t n_points_granero =13;
-Float_t x1[n_points_granero], y1[n_points_granero];
-Float_t x, y;
-Int_t ncols_granero;
+//FILE *fg1=fopen("granero.txt", "r");
+//Int_t n_points_granero =13;
+//Float_t x1[n_points_granero], y1[n_points_granero];
+//Float_t x, y;
+//Int_t ncols_granero;
+//Int_t nlines1 =0;
+
+//while(1)
+// {
+//  ncols_granero = fscanf(fg1,"%f %f",&x, &y);
+//  if (ncols_granero<0) break;
+//  //std::cout << "x " << x << std::endl;
+//  x1[nlines1]=x;
+//  y1[nlines1]=y;
+//  //std::cout << "y " << y << std::endl;
+//  nlines1++;
+//}
+
+FILE *fg1=fopen("Donaire.txt", "r");
+Int_t n_points_donaire =200;
+Float_t x1[n_points_donaire], y1[n_points_donaire], z1[n_points_donaire];
+Float_t x, y, z;
+Int_t ncols_donaire;
 Int_t nlines1 =0;
 
 while(1)
  {
-  ncols_granero = fscanf(fg1,"%f %f",&x, &y);
-  if (ncols_granero<0) break;
- // std::cout << "x " << x << std::endl;
+  ncols_donaire = fscanf(fg1,"%f %f %f",&x, &y,&z);
+  if (ncols_donaire<0) break;
+  //std::cout << "x " << x << std::endl;
   x1[nlines1]=x;
-  y1[nlines1]=y;
+  y1[nlines1]=y/1.111;
+  z1[nlines1]=z;
+  std::cout << "x" << x << "y " << y1[nlines1] << "z" << z <<std::endl;
   nlines1++;
 }
 
@@ -39,7 +59,8 @@ while(1)
 
 fclose(fg2);
 
-TGraph *gr1 = new TGraph (nlines1, x1, y1);
+//TGraph *gr1 = new TGraph (nlines1, x1, y1);
+TGraphErrors *gr1 = new TGraphErrors(nlines1,x1,y1,z1,z1);
 TGraph *gr2 = new TGraph (nlines2, x2, y2);
 
 TCanvas *c1 = new TCanvas("c1","Graph Draw Options",
@@ -49,25 +70,26 @@ gPad->SetLogy();
 
 // draw the graph with axis, continuous line, and put
 // a * at each point
-gr1->SetTitle("Dose rate distribution");
-gr1-> GetXaxis()->SetTitle("Distance from the centre (cm)");
-gr1->GetYaxis()->SetTitle("Normalised dose rate distribution");
+gr1->SetTitle("Radial dose deposition");
+gr1-> GetXaxis()->SetTitle("r (cm)");
+gr1->GetYaxis()->SetTitle("Normalised dose rate (z=0)");
 gr1->SetLineWidth(1);
 gr1->SetMarkerColor(1);
-gr1->SetMarkerStyle(20);
+gr1->SetMarkerStyle(2);
+gr1->SetMarkerSize(0.5);
 gr1->Draw("AP");
 
 gr2->SetLineWidth(1);
 gr2->SetMarkerColor(2);
-gr2->SetMarkerStyle(21);
+gr2->SetMarkerStyle(2);
 gr2->SetMarkerSize(0.5);
 gr2->SetLineColor(2);
 gr2->Draw("CP");
 
-TLegend *leg = new TLegend(0.3, 0.5, 0.6, 0.8);
+TLegend *leg = new TLegend(0.6, 0.7, 0.85, 0.85);
 leg->SetFillColor(0);
 leg->AddEntry(gr1, "Reference data", "lp");
-leg->AddEntry(gr2, "Geant4 - 280 M events", "lp");
+leg->AddEntry(gr2, "Geant4 - 280e6 events", "lp");
 leg->Draw();
 
 
